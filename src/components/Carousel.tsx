cbec61b4e3bbd00409ac5d081a3fc87aa23312d1
Victor Filgueira <victor.filgueira@mindsatwork.com.br>
2023-09-01 18:02:20 -0300

    feat: grid

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,16 @@
+
+
+const Carousel = () => {
+
+  return (
+    <div className="flex flex-col items-center justify-center px-9 w-full">
+      <p className="text-center text-white max-w-[16rem] lg:max-w-[23.5rem]">
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper, tortor sed vehicula."
+      </p>
+      <p className="text-white mt-3">
+        @victorpbf
+      </p>
+    </div>
+  );
+};
+export default Carousel;
