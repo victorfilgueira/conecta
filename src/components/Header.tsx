c726bb18a25ede87a1051d01a100afbc8a5a7c94
Victor Filgueira <victor.filgueira@mindsatwork.com.br>
2023-08-31 21:28:45 -0300

    style: top header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,22 +3,24 @@ import bg from '@/assets/bg-image.jpg';
 import logo from '@/assets/conecta-logo.png';
 import fb from '@/assets/facebook.png';
 import ig from '@/assets/instagram.png';
+import Link from 'next/link';
 
 export function Header() {
+
     return(
-        <header className="bg-scroll bg-cover bg-center h-[750px]"
+        <header className="bg-cover w-screen h-screen"
             style={{backgroundImage: `url(${bg.src})`}}
         >
             <div className='pt-5'>
-                <div className='h-10 flex justify-center'>
+                <div className="h-10 flex justify-between items-center ml-[12vw] mr-[12vw]">
                     <Image src={logo.src} alt='Conecta Logo' quality={100} width={134} height={51}/>
-                    <div className='flex w-14 h-10 ml-[960px] items-center justify-center'>
-                        <button className='mr-3 h-10'>
+                    <div className='flex h-10 items-center'>
+                        <Link href='#' className='mr-3 h-10 flex items-center justify-center'>
                             <Image src={fb.src} alt='Facebook Logo' width={18} height={18}/>
-                        </button>
-                        <button className='ml-3 h-10'>
+                        </Link>
+                        <Link href='#' className='ml-3 h-10 flex items-center justify-center'>
                             <Image src={ig.src} alt='Instagram Logo' width={18} height={18}/>  
-                        </button>
+                        </Link>
                     </div>
                 </div>
             </div>
